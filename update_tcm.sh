930d2d84cf70b3da48846709ec7e3ae2b0e70021
Thomas Nelson <tanelso2@gmail.com>
2022-09-22 15:20:52 -0500

    Make sure to always connect to https when updating tcm

--- update_tcm.sh
+++ update_tcm.sh
@@ -9,4 +9,4 @@ export FULL_DOCKER_TAG="$GCR_REPO/$REPO_NAME:$IMAGE_TAG"
 curl -X PATCH \
     -H "Authorization: Basic ${TCM_KEY}" \
     -d "$FULL_DOCKER_TAG" \
-    ${TCM_ADDR}/container/${REPO_NAME}
+    https://${TCM_ADDR}/container/${REPO_NAME}
