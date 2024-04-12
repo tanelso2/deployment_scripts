fed07779c14ca4b299bcb75187883ba4ef0a2231
Thomas Nelson <tanelso2@gmail.com>
2024-04-12 18:29:12 -0500

    Add action for building docker image and pushing to ECR

---
+++ push_image_to_ecr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+export IMAGE_TAG=gh-$(git rev-parse --short HEAD)
+
+export FULL_DOCKER_TAG="$ECR_REPO/$REPO_NAME:$IMAGE_TAG"
+
+docker build -t $REPO_NAME:latest .
+
+docker tag $REPO_NAME:latest $FULL_DOCKER_TAG
+docker push $FULL_DOCKER_TAG
+
