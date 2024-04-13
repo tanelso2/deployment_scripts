c8ce5a0da258d3fd6d15ee10d1dbe48b7da46992
Thomas Nelson <tanelso2@gmail.com>
2024-04-12 20:28:51 -0500

    Add image tag output to ecr action

--- actions/build_and_push_to_ecr/push_image_to_ecr.sh
+++ actions/build_and_push_to_ecr/push_image_to_ecr.sh
@@ -7,6 +7,8 @@ export IMAGE_TAG=gh-$(git rev-parse --short HEAD)
 
 export FULL_DOCKER_TAG="$ECR_REPO/$REPO_NAME:$IMAGE_TAG"
 
+echo "tag=${FULL_DOCKER_TAG}" >> $GITHUB_OUTPUT
+
 docker build -t $REPO_NAME:latest .
 
 docker tag $REPO_NAME:latest $FULL_DOCKER_TAG
